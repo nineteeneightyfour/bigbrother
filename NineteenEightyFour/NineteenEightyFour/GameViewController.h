da7304c8f752eabaf2248aa11fea3059b979e9b2
Stéphane Hanser <shanser@captive-studio.com>
2011-04-23 00:22:27 +0200

    GameViewController created

---
+++ NineteenEightyFour/NineteenEightyFour/GameViewController.h
@@ -0,0 +1,16 @@
+//
+//  GameViewController.h
+//  NineteenEightyFour
+//
+//  Created by Stéphane Hanser on 23/04/11.
+//  Copyright 2011 Jonathan Perret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GameViewController : UIViewController {
+    
+}
+
+@end
